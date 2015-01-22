0587ae43ac1010af7a402a67effae81922e7d8bc
Denley Bihari <denley@denley.me>
2015-01-22 17:02:51 +1030

    Initial commit - Hello World app

---
+++ app/src/main/java/me/denley/denleybihari/MainActivity.java
@@ -0,0 +1,14 @@
+package me.denley.denleybihari;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
