5340d5a6f3e2eaad81fa0cf607d313bfe58ebca8
Denley Bihari <denley@denley.me>
2015-01-28 14:29:21 +1030

    Added more button to testimonials page, tweaked alignment

--- app/src/main/java/me/denley/denleybihari/TestimonialsView.java
+++ app/src/main/java/me/denley/denleybihari/TestimonialsView.java
@@ -1,10 +1,15 @@
 package me.denley.denleybihari;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A custom View representing the "Testimonials" page.
@@ -13,10 +18,25 @@ import butterknife.ButterKnife;
  */
 public class TestimonialsView extends FrameLayout {
 
+    private static final String URL_MORE = "https://www.elance.com/s/denleybihari/job-history";
+
+
     public TestimonialsView(Context context) {
         super(context);
         LayoutInflater.from(context).inflate(R.layout.page_testimonials, this);
         ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.action_more_testimonials)
+    void onMoreClick(){
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(URL_MORE));
+
+        try{
+            getContext().startActivity(intent);
+        }catch(ActivityNotFoundException e){
+            Toast.makeText(getContext(), R.string.error_cant_open, Toast.LENGTH_LONG).show();
+        }
     }
 
 }
